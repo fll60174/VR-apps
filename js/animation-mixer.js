392af311db89231e4f124aa6a1f3d7be33077a3f
Ruibing Hao <Ruibing_Hao@cable.comcast.com>
2024-01-10 13:48:36 -0500

    modelviewer

--- js/animation-mixer.js
+++ js/animation-mixer.js
@@ -1,146 +1,80 @@
-const LoopMode = {
-  once: THREE.LoopOnce,
-  repeat: THREE.LoopRepeat,
-  pingpong: THREE.LoopPingPong
-};
+/* global AFRAME, THREE */
 
-/**
- * animation-mixer
- *
- * Player for animation clips. Intended to be compatible with any model format that supports
- * skeletal or morph animations through THREE.AnimationMixer.
- * See: https://threejs.org/docs/?q=animation#Reference/Animation/AnimationMixer
- */
+var DEFAULT_CLIP = '__auto__';
+
 AFRAME.registerComponent('animation-mixer', {
   schema: {
-    clip:  {default: '*'},
-    duration: {default: 0},
-    clampWhenFinished: {default: false, type: 'boolean'},
-    crossFadeDuration: {default: 0},
-    loop: {default: 'repeat', oneOf: Object.keys(LoopMode)},
-    repetitions: {default: Infinity, min: 0},
-    timeScale: {default: 1}
+    clip: {default: DEFAULT_CLIP},
+    duration: {default: 0}
   },
 
   init: function () {
-    /** @type {THREE.Mesh} */
-    this.model = null;
-    /** @type {THREE.AnimationMixer} */
-    this.mixer = null;
-    /** @type {Array<THREE.AnimationAction>} */
-    this.activeActions = [];
+    var model = this.el.getObject3D('mesh');
 
-    const model = this.el.getObject3D('mesh');
+    this.model = null;
+    this.mixer = null;
+    this.activeAction = null;
 
     if (model) {
       this.load(model);
     } else {
-      this.el.addEventListener('model-loaded', (e) => {
+      this.el.addEventListener('model-loaded', function (e) {
         this.load(e.detail.model);
-      });
+      }.bind(this));
     }
   },
 
   load: function (model) {
-    const el = this.el;
     this.model = model;
     this.mixer = new THREE.AnimationMixer(model);
-    this.mixer.addEventListener('loop', (e) => {
-      el.emit('animation-loop', {action: e.action, loopDelta: e.loopDelta});
-    });
-    this.mixer.addEventListener('finished', (e) => {
-      el.emit('animation-finished', {action: e.action, direction: e.direction});
-    });
-    if (this.data.clip) this.update({});
+    if (this.data.clip) { this.update({}); }
   },
 
   remove: function () {
     if (this.mixer) this.mixer.stopAllAction();
   },
 
-  update: function (prevData) {
-    if (!prevData) return;
+  update: function (previousData) {
+    if (!previousData) return;
 
-    const data = this.data;
-    const changes = AFRAME.utils.diff(data, prevData);
+    var data = this.data;
 
-    // If selected clips have changed, restart animation.
-    if ('clip' in changes) {
-      this.stopAction();
-      if (data.clip) this.playAction();
+    if (data.clip !== previousData.clip) {
+      if (this.activeAction) this.activeAction.stop();
+      if (data.clip) this.playClip(data.clip);
+    }
+
+    if (!this.activeAction) return;
+
+    if (data.duration) {
+      this.activeAction.setDuration(data.duration);
+    }
+  },
+
+  playClip: function (clipName) {
+    if (!this.mixer) return;
+
+    var clip;
+    var data = this.data;
+    var model = this.model;
+    var animations = model.animations || (model.geometry || {}).animations || [];
+
+    if (!animations.length) { return; }
+
+    clip = clipName === DEFAULT_CLIP
+      ? animations[0]
+      : THREE.AnimationClip.findByName(animations, data.clip);
+
+    if (!clip) {
+      console.error('[animation-mixer] Clip "%s" not found.', data.clip);
       return;
     }
 
-    // Otherwise, modify running actions.
-    this.activeActions.forEach((action) => {
-      if ('duration' in changes && data.duration) {
-        action.setDuration(data.duration);
-      }
-      if ('clampWhenFinished' in changes) {
-        action.clampWhenFinished = data.clampWhenFinished;
-      }
-      if ('loop' in changes || 'repetitions' in changes) {
-        action.setLoop(LoopMode[data.loop], data.repetitions);
-      }
-      if ('timeScale' in changes) {
-        action.setEffectiveTimeScale(data.timeScale);
-      }
-    });
-  },
-
-  stopAction: function () {
-    const data = this.data;
-    for (let i = 0; i < this.activeActions.length; i++) {
-      data.crossFadeDuration
-        ? this.activeActions[i].fadeOut(data.crossFadeDuration)
-        : this.activeActions[i].stop();
-    }
-    this.activeActions.length = 0;
-  },
-
-  playAction: function () {
-    if (!this.mixer) return;
-
-    const model = this.model,
-        data = this.data,
-        clips = model.animations || (model.geometry || {}).animations || [];
-
-    if (!clips.length) return;
-
-    const re = wildcardToRegExp(data.clip);
-
-    for (let clip, i = 0; (clip = clips[i]); i++) {
-      if (clip.name.match(re)) {
-        const action = this.mixer.clipAction(clip, model);
-        action.enabled = true;
-        action.clampWhenFinished = data.clampWhenFinished;
-        if (data.duration) action.setDuration(data.duration);
-        if (data.timeScale !== 1) action.setEffectiveTimeScale(data.timeScale);
-        action
-          .setLoop(LoopMode[data.loop], data.repetitions)
-          .fadeIn(data.crossFadeDuration)
-          .play();
-        this.activeActions.push(action);
-      }
-    }
+    this.activeAction = this.mixer.clipAction(clip, model);
+    this.activeAction.play();
   },
 
   tick: function (t, dt) {
     if (this.mixer && !isNaN(dt)) this.mixer.update(dt / 1000);
   }
 });
-
-/**
- * Creates a RegExp from the given string, converting asterisks to .* expressions,
- * and escaping all other characters.
- */
-function wildcardToRegExp (s) {
-  return new RegExp('^' + s.split(/\*+/).map(regExpEscape).join('.*') + '$');
-}
-
-/**
- * RegExp-escapes all characters in the given string.
- */
-function regExpEscape (s) {
-  return s.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
-}
